7f9b682ba773b4800ad6ced7032d87fc49e61a8f
DanG-11 <daniil.gavrilov@teb.edu.pl>
2025-12-10 11:03:58 +0100

    Zadanie 5 wykonane

--- ZadanieJasinskiManimulacjaTekstem/Program.cs
+++ ZadanieJasinskiManimulacjaTekstem/Program.cs
@@ -1,73 +1,85 @@
-﻿//Zad 1
-Console.WriteLine("Wpisz tekst:");
-string OGTekst = Console.ReadLine();
-int DlugoscTekstu = Convert.ToInt32(OGTekst.Length);
-string TekstMalymi = OGTekst.ToLower();
-string TekstDuzymi = OGTekst.ToUpper();
+﻿////Zad 1
+//Console.WriteLine("Wpisz tekst:");
+//string OGTekst = Console.ReadLine();
+//int DlugoscTekstu = Convert.ToInt32(OGTekst.Length);
+//string TekstMalymi = OGTekst.ToLower();
+//string TekstDuzymi = OGTekst.ToUpper();
 
-Console.WriteLine(DlugoscTekstu);
-Console.WriteLine(TekstMalymi);
-Console.WriteLine(TekstDuzymi);
+//Console.WriteLine(DlugoscTekstu);
+//Console.WriteLine(TekstMalymi);
+//Console.WriteLine(TekstDuzymi);
 
-//Zad 1.1, 1.2
-Console.WriteLine("1 - Wypisz tekst z małych liter; 2 - Wypisz tekst z dużych liter; 3 - Pierwsza litera duża a reszta małe.");
-int WyborTekstu = Convert.ToInt32(Console.ReadLine());
+////Zad 1.1, 1.2
+//Console.WriteLine("1 - Wypisz tekst z małych liter; 2 - Wypisz tekst z dużych liter; 3 - Pierwsza litera duża a reszta małe.");
+//int WyborTekstu = Convert.ToInt32(Console.ReadLine());
 
-if (WyborTekstu == 1)
-{
-    Console.WriteLine(TekstMalymi);
-}
-else if (WyborTekstu == 2)
-{
-    Console.WriteLine(TekstDuzymi);
-}
-else if (WyborTekstu == 3)
-{
-    string[] OddzielneLiteryTekstu = TekstDuzymi.Split();
-    Console.WriteLine(TekstDuzymi.Substring(0, 1) + TekstMalymi.Substring(1));
-}
-else
-{
-    Console.WriteLine("Niepoprawny wybór.");
-}
+//if (WyborTekstu == 1)
+//{
+//    Console.WriteLine(TekstMalymi);
+//}
+//else if (WyborTekstu == 2)
+//{
+//    Console.WriteLine(TekstDuzymi);
+//}
+//else if (WyborTekstu == 3)
+//{
+//    string[] OddzielneLiteryTekstu = TekstDuzymi.Split();
+//    Console.WriteLine(TekstDuzymi.Substring(0, 1) + TekstMalymi.Substring(1));
+//}
+//else
+//{
+//    Console.WriteLine("Niepoprawny wybór.");
+//}
 
-//Zad 2
-Console.WriteLine("Wpisz tekst:");
-string OGTekst2 = Console.ReadLine();
-string TekstBezSpacji = OGTekst2.Trim();
-int DlugoscTekstuBezSpacji = Convert.ToInt32(TekstBezSpacji.Length);
-string PierwszaLiteraTekstuBezSpacji = TekstBezSpacji.Substring(0, 1);
-string OstatniaLiteraTekstuBezSpacji = TekstBezSpacji.Substring(DlugoscTekstuBezSpacji - 1, 1);
+////Zad 2
+//Console.WriteLine("Wpisz tekst:");
+//string OGTekst2 = Console.ReadLine();
+//string TekstBezSpacji = OGTekst2.Trim();
+//int DlugoscTekstuBezSpacji = Convert.ToInt32(TekstBezSpacji.Length);
+//string PierwszaLiteraTekstuBezSpacji = TekstBezSpacji.Substring(0, 1);
+//string OstatniaLiteraTekstuBezSpacji = TekstBezSpacji.Substring(DlugoscTekstuBezSpacji - 1, 1);
 
-Console.WriteLine("Tekst bez spacji na początku i końcu: " + TekstBezSpacji);
-Console.WriteLine("Długość tekstu: " + DlugoscTekstuBezSpacji);
-Console.WriteLine("Pierwsza litera tekstu: " + PierwszaLiteraTekstuBezSpacji);
-Console.WriteLine("Ostatnia litera tekstu: " + OstatniaLiteraTekstuBezSpacji);
+//Console.WriteLine("Tekst bez spacji na początku i końcu: " + TekstBezSpacji);
+//Console.WriteLine("Długość tekstu: " + DlugoscTekstuBezSpacji);
+//Console.WriteLine("Pierwsza litera tekstu: " + PierwszaLiteraTekstuBezSpacji);
+//Console.WriteLine("Ostatnia litera tekstu: " + OstatniaLiteraTekstuBezSpacji);
 
-//Zad 3
-Console.WriteLine("Wpisz tekst:");
-string OGTekst3 = Console.ReadLine();
-Console.WriteLine("Jakie znaki wyliczyć:");
-char JakiZnak = Console.ReadKey().KeyChar;
+////Zad 3
+//Console.WriteLine("Wpisz tekst:");
+//string OGTekst3 = Console.ReadLine();
+//Console.WriteLine("Jakie znaki wyliczyć:");
+//char JakiZnak = Console.ReadKey().KeyChar;
 
-int zad3occurences = 0;
+//int zad3occurences = 0;
 
-for (int x = 0; x < OGTekst3.Length; x++)
-{
-    if (JakiZnak == OGTekst3[x])
-    {
-        zad3occurences++;
-    }
-}
-Console.WriteLine($"\nLitera: {JakiZnak}, wystepuje {zad3occurences} razy.");
+//for (int x = 0; x < OGTekst3.Length; x++)
+//{
+//    if (JakiZnak == OGTekst3[x])
+//    {
+//        zad3occurences++;
+//    }
+//}
+//Console.WriteLine($"\nLitera: {JakiZnak}, wystepuje {zad3occurences} razy.");
 
-//Zad 4
+////Zad 4
 
+//Console.WriteLine("Wypisz zdanie:");
+//string OGTekst4 = Console.ReadLine();
+//Console.WriteLine("Jaki znak chcesz zamienić?");
+//string UserReplacementInputFrom = Convert.ToString(Console.ReadLine());
+//Console.WriteLine("Na jaki?");
+//string UserReplacementInputTo = Convert.ToString(Console.ReadLine());
+//string ReplacedText = OGTekst4.Replace(UserReplacementInputFrom, UserReplacementInputTo);
+//Console.WriteLine(ReplacedText);
+
+//Zad 5
 Console.WriteLine("Wypisz zdanie:");
-string OGTekst4 = Console.ReadLine();
-Console.WriteLine("Jaki znak chcesz zamienić?");
-string UserReplacementInputFrom = Convert.ToString(Console.ReadLine());
-Console.WriteLine("Na jaki?");
-string UserReplacementInputTo = Convert.ToString(Console.ReadLine());
-string ReplacedText = OGTekst4.Replace(UserReplacementInputFrom, UserReplacementInputTo);
-Console.WriteLine(ReplacedText);
+string OGTekst5 = Console.ReadLine();
+char[] OGTekst5Array = OGTekst5.ToCharArray();
+string TekstOdwrocony = string.Empty;
+for (int i = 0; i < OGTekst5.Length; i++)
+{
+    
+    TekstOdwrocony = TekstOdwrocony + OGTekst5Array[OGTekst5.Length - 1 - i];
+}
+Console.WriteLine(TekstOdwrocony);
